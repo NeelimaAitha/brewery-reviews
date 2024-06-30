b933c8f4a45db8cddde197ce8a6bd64c422f77db
NeelimaAitha <138878450+NeelimaAitha@users.noreply.github.com>
2024-06-30 20:27:23 +0530

    Your commit message here

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const sequelize = require('./config/db'); // Assuming you have a config file for Sequelize
 const bcrypt = require('bcrypt'); // For password hashing
